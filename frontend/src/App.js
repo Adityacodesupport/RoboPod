c91c24cb6e349cfa5751c028a24298209b4ac4d3
AdityaCodeSupport <AdityaKhatode1403@gmail.com>
2024-01-05 13:14:22 +0530

    First Phase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import SelectDeploymentType from './Pages/Deployment/SelectDeploymentType/SelectDeploymentType'
+import Home from './Pages/HomePage/Home'
+import React from 'react'
+import Login from './Pages/Login/Login'
+import Register from './Pages/Register/Register'
+import Main from './Pages/MainPage/Main'
+import { BrowserRouter as Router,Routes,Route,Link,Redirect} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { LogIn, LogOut} from './features/User/userSlice'
+import { Navigate } from 'react-router-dom'
+import DeployAppOpenSource from './Pages/Deployment/DeployAppOpenSource/DeployAppOpenSource'
+import DeployAppAWS from './Pages/Deployment/DeployAppAWS/DeployAppAWS'
+import DeployAppOpenShift from './Pages/Deployment/DeployAppOpenShift/DeployAppOpenshift'
 
-function App() {
+const App = () => {
+  const isLoggedIn = useSelector((state)=>state.user.LoggedIn)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={
+          isLoggedIn?<Navigate to='/main' replace={true}/>:<Home />}/>
+          <Route exact path='/login' element={isLoggedIn ? 
+          (<Navigate to='/main' replace={true} />
+          ) : (
+            <Login />
+          )}/>
+          <Route exact path='/register' element={isLoggedIn?<Navigate to='/main'/>:<Register/>}></Route>
+          <Route exact path='/main' element={isLoggedIn?<Main />:<Navigate to='/register'/>}></Route>
+          <Route exact path='/deployment-select' element={isLoggedIn?<SelectDeploymentType />:<Navigate to='/register'/>}></Route>
+          <Route exact path='/deployment/deploy-app-opensorce' element={isLoggedIn?<DeployAppOpenSource />:<Navigate to='/register'/>}></Route>
+          <Route exact path='/deployment/deploy-app-aws' element={isLoggedIn?<DeployAppAWS />:<Navigate to='/register'/>}></Route>
+          <Route exact path='/deployment/deploy-app-openshift' element={isLoggedIn?<DeployAppOpenShift />:<Navigate to='/register'/>}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
