ce9364aad2b9d3d5b627202e06acc5a20e365ec4
AdityaCodeSupport <AdityaKhatode1403@gmail.com>
2024-01-11 19:20:51 +0530

    popup slight change

--- frontend/src/components/Popup/Popup.jsx
+++ frontend/src/components/Popup/Popup.jsx
@@ -2,14 +2,14 @@
 import React, { useState } from "react";
 import "./Popup.css"; // Import the CSS file for styling
 import axios from "axios";
-import Box from '@mui/material/Box';
-import LinearProgress from '@mui/material/LinearProgress';
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
 
-const Popup = ({ selectedAppData,appName, onClose }) => {
-  const [progress,setProgress]=useState(0)
+const Popup = ({ selectedAppData, appName, onClose }) => {
+  const [progress, setProgress] = useState(0);
   if (!selectedAppData) return null;
 
-  const getPodDetail = ({podName}) => {
+  const getPodDetail = ({ podName }) => {
     const timer = setInterval(() => {
       setProgress((oldProgress) => {
         if (oldProgress === 100) {
@@ -19,19 +19,20 @@ const Popup = ({ selectedAppData,appName, onClose }) => {
         return Math.min(oldProgress + diff, 100);
       });
     }, 250);
-    axios.get(`http://localhost:3001/api/desc_pod/${podName}`)
-    .then((res)=>{
-      clearInterval(timer);
-      setProgress(0)
-      alert('success')
-      console.log(res.data)
-    })
-    .catch((err)=>{
-      clearInterval(timer);
-      setProgress(0)      
-      alert(err.message)
-    })
-  }
+    axios
+      .get(`http://localhost:3001/api/desc_pod/${podName}`)
+      .then((res) => {
+        clearInterval(timer);
+        setProgress(0);
+        alert("success");
+        console.log(res.data);
+      })
+      .catch((err) => {
+        clearInterval(timer);
+        setProgress(0);
+        alert(err.message);
+      });
+  };
 
   return (
     <div className="popup-container">
@@ -40,28 +41,36 @@ const Popup = ({ selectedAppData,appName, onClose }) => {
         <h3>Pod Details:</h3>
         {/* <h3>APP DETAILS: {selectedAppData.appname}</h3> */}
         {/* {selectedAppData} */}
-        <Box sx={{ width: '100%' }}>
-      <LinearProgress variant="determinate" value={progress} />
-    </Box>
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress variant="determinate" value={progress} />
+        </Box>
         <table>
-      <thead>
-        <tr>
-          <th>POD NAME</th>
-          <th>STATUS</th>
-          <th>CREATION TIME</th>
-        </tr>
-      </thead>
-      <tbody>
-        {selectedAppData.podNames.map((podName, index) => (
-          <tr key={podName}>
-            <td className="pop-up-appName" onClick={()=>getPodDetail({podName})} style={{ cursor: 'pointer' }}>{podName}</td>
-            <td>{selectedAppData.podStatus[index]}</td>
-            <td>{selectedAppData.creationTime[index]}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-        <button className="popup-btn" onClick={onClose}>CLOSE</button>
+          <thead>
+            <tr>
+              <th>POD NAME</th>
+              <th>STATUS</th>
+              <th>CREATION TIME</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedAppData.podNames.map((podName, index) => (
+              <tr key={podName}>
+                <td
+                  className="pop-up-appName"
+                  onClick={() => getPodDetail({ podName })}
+                  style={{ cursor: "pointer" }}
+                >
+                  {podName}
+                </td>
+                <td>{selectedAppData.podStatus[index]}</td>
+                <td>{selectedAppData.creationTime[index]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button className="popup-btn" onClick={onClose}>
+          CLOSE
+        </button>
       </div>
     </div>
   );
